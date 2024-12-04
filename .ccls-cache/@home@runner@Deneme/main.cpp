2daffbdfb5657302d3704d9400ef6d7bdf488540
Oray Yılmaz <orayyilmaz16@gmail.com>
2024-12-04 20:21:59 +0000

    Sayı İsteme ve Toplama fonksiyonu yapıldı

--- .ccls-cache/@home@runner@Deneme/main.cpp
+++ .ccls-cache/@home@runner@Deneme/main.cpp
@@ -15,10 +15,26 @@ bool karsilastir(bool dogruluk) {
   }
 }
 
+int sayiIsteveHesapla(void){
+  int sayi1,sayi2;
+  cout << "1.Sayi: "; 
+  cin >> sayi1;
+  cout << endl;
+  cout << "2.Sayi: ";
+  cin >> sayi2;
+  cout << endl;
+
+  return sayi1 + sayi2;
+}
+
+
+
 int main(void) {
 
   setlocale(LC_ALL, "Turkish");
-  
+
+  cout << sayiIsteveHesapla();
+   
 
   
   return 0;
