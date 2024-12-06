cd42103f2a57eadfeb0206ceb232aca07c053a4d
Oray Yılmaz <orayyilmaz16@gmail.com>
2024-12-06 17:16:44 +0000

    Class methodu calistirildi.

--- .ccls-cache/@home@runner@Deneme/main.cpp
+++ .ccls-cache/@home@runner@Deneme/main.cpp
@@ -3,39 +3,34 @@
 #include <time.h>
 using namespace std;
 
-string adiSoyadiYazdir(string isim, string soyisim) {
-  return isim + " " + soyisim;
-}
+class Oray{
 
-bool karsilastir(bool dogruluk) {
-  if (dogruluk == true) {
-    return true;
-  } else {
-    return false;
+   public:
+      int model;
+      string color;
+
+  
+  void ModelSorgula(){
+    cout << "Araba Modeli: " << model << endl; 
   }
-}
 
-int sayiIsteveHesapla(void){
-  int sayi1,sayi2;
-  cout << "1.Sayi: "; 
-  cin >> sayi1;
-  cout << endl;
-  cout << "2.Sayi: ";
-  cin >> sayi2;
-  cout << endl;
+  void RengiSorgula(){
+    cout << "Araba rengi:" << color << endl;
+  }
 
-  return sayi1 + sayi2;
-}
-
+         
+};
 
 
 int main(void) {
 
   setlocale(LC_ALL, "Turkish");
 
-  cout << sayiIsteveHesapla();
-   
-
-  
+  Oray araba;
+  araba.model = 2013;
+  araba.color = "Siyah";
+  araba.ModelSorgula();
+  araba.RengiSorgula();
+  cout << endl;
   return 0;
 }
